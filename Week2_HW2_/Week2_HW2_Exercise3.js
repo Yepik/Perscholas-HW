2c3948b24ce323209c7be16bac56b264af794640
Jaroslaw Pogoda <jarek1330@gmail.com>
2021-12-10 14:42:44 -0500

    updated and cleaned up code

--- Week2_HW2_/Week2_HW2_Exercise3.js
+++ Week2_HW2_/Week2_HW2_Exercise3.js
@@ -1,11 +1,13 @@
 //Write a program to find the longest element in a string array.
-inputValue=document.getElementById('inputAttribute_Exercise3').value.split(' ')
-let longestElement = ""
-let longestElementIndex = 0
-const findLongestStringElement = inputArray=> inputArray.map((element, index)=> {
-    if(longestElement.length<element.length){
-        longestElementIndex=index
-        longestElement=element
-    }
+const findLongestStringElement = inputArray=>{
+    let longestElement = ""
+    let longestElementIndex = 0
+    let indexCounter = -1
+    return inputArray.map((element, index)=> {
+        indexCounter++
+        if(longestElement.length<element.length){
+            longestElementIndex=indexCounter
+            longestElement=element
+        }
     document.getElementById('displayAttribute_exercise3').innerHTML=`The longest string is ${longestElement} and it is at index ${longestElementIndex}`
-})
+})}
