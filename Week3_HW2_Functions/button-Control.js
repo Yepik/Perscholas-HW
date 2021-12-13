c069ad4b8dc3bd7c49a9fcf264c2117a90338386
Jaroslaw Pogoda <jarek1330@gmail.com>
2021-12-12 19:44:43 -0500

    updatee

--- Week3_HW2_Functions/button-Control.js
+++ Week3_HW2_Functions/button-Control.js
@@ -1,9 +1,17 @@
-const clickMeToRevealProblem=(tagClass)=>{
+const clickMeToRevealProblem=(tagClass,tagId)=>{
     for(let i=0;i<document.getElementsByClassName(tagClass).length;i++)
     {
         document.getElementsByClassName(tagClass)[i].style.display='flex'
     }
+    document.getElementById(tagId).style.display="flex"
     };
+    const clickMeToHideProblem=(tagClass,tagId)=>{
+        for(let i=0;i<document.getElementsByClassName(tagClass).length;i++)
+        {
+            document.getElementsByClassName(tagClass)[i].style.display='none'
+        }
+        document.getElementById(tagId).style.display="flex"
+        };
 const turnOffTheButton = tagId=> document.getElementById(tagId).style.display="none"
 //not working as intended
 // const printToHtMLOptionForDropdown = (studentArray, idTag) =>{
