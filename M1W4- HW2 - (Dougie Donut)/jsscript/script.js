12064f4cd07b795d4204a6307f23a031916c9ac0
Jaroslaw Pogoda <jarek1330@gmail.com>
2021-12-17 18:48:56 -0500

    updating homework

--- M1W4- HW2 - (Dougie Donut)/jsscript/script.js
+++ M1W4- HW2 - (Dougie Donut)/jsscript/script.js
@@ -4,7 +4,7 @@ const boroughsArray = [
   "Brooklyn",
   "Manhattan",
   "Queens",
-  "Staten Island",
+  "staten island",
 ];
 const agency = "NYPD";
 const textAreaInput = document.getElementById("inputEvent");
@@ -13,35 +13,68 @@ class boroughs {
 
   constructor(boroughInput, agencyInput) {
     this.borough = boroughInput;
-    if("STATEN ISLAND"==boroughInput.toUpperCase()){boroughInput='statenisland'}
+    this.temp_borough=boroughInput
+    if("STATEN ISLAND".toLowerCase()==this.temp_borough){
+        this.temp_borough='statenisland'
+        }
     this.div = document.getElementById(
       "buttons"
-    ).innerHTML += `<button id="${boroughInput}" onclick="${boroughInput.toLowerCase()}.fetchingData(${boroughInput.toLowerCase()}.borough,${this.borough})">${boroughInput}</button>`;
-    this.button = document.getElementById(`${boroughInput}`);
+    ).innerHTML += `<button id="${this.temp_borough}" onclick="${this.temp_borough.toLowerCase()}.fetchingData(${this.temp_borough.toLowerCase()}.borough,'${boroughInput}')">${this.temp_borough}</button>`;
+    this.button = document.getElementById(`${this.temp_borough}`);
     this.borough = boroughInput;
     this.agency = agencyInput;
     // this.eventListener = this.button.addEventListener("click",this.fetchingData(boroughInput))
   }
+  compare( a, b ) {
+    if ( a.last_nom < b.last_nom ){
+      return -1;
+    }
+    if ( a.last_nom > b.last_nom ){
+      return 1;
+    }
+    return 0;
+  }
+  
+  
   fetchingData(boroughInput,boroughName) {
     let url = `https://data.cityofnewyork.us/resource/erm2-nwe9.json?agency=${
       this.agency
     }&borough=${boroughName.toUpperCase()}`;
-    console.log(url);
     const textAreaOutput = document.getElementById("output");
-    //if("STATEN ISLAND"==boroughInput.toUpperCase()){boroughInput='statenisland'}
     
+    if("STATEN ISLAND".toLowerCase()==boroughInput){
+        boroughInput='statenisland'
+        }
     fetch(url)
       .then((response) => response.json())
-      .then((data) => {
+      .then((data) => {console.log(data)
         textAreaOutput.innerHTML = "";
+        let tempArray =[]
+        let latitude =[]
+        let longitude =[]
+        for (let i = 0; i < textAreaInput.value; i++) {
+            tempArray.push({descriptor:data[i].descriptor, resolution:data[i].resolution_description})
+            latitude.push(data[i].latitude)
+            longitude.push(data[i].longitude)
+        }
+        console.log(latitude, longitude)
+        let tempArray2=tempArray.sort(( a, b )=> {
+            if ( a.descriptor < b.descriptor ){
+              return -1;
+            }
+            if ( a.descriptor > b.descriptor ){
+              return 1;
+            }
+            return 0;
+          })
         for (let i = 0; i < textAreaInput.value; i++) {
           textAreaOutput.innerHTML += `<div ><table id="row${i}"><tr><td>${
-            data[i].descriptor
+            tempArray[i].descriptor
           }</td><td><button type="button" onclick="${boroughInput.toLowerCase()}.responseMessage('${
-            data[i].resolution_description
+            tempArray[i].resolution
           }','row${i}')">What did police do?</button></td></tr></table></div>`;
         }
-        textAreaOutput.innerHTML += "";
+        textAreaOutput.innerHTML+= "";
       });
   }
   responseMessage(responseMessage, element) {
