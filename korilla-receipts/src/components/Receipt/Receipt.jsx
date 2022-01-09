0fb6a71325809331e66cf0b47a6d6e8cafaeb2f3
Jaroslaw Pogoda <jarek1330@gmail.com>
2022-01-09 17:34:55 -0500

    added

--- korilla-receipts/src/components/Receipt/Receipt.jsx
+++ korilla-receipts/src/components/Receipt/Receipt.jsx
@@ -1,14 +1,16 @@
 import './Receipt.css'
 function Receipt(props) {
   
-  
+  console.log(props)
   return (
       <div className="Receipt">
       <h2 className="ReceiptName"><span className="ReceiptDataText">{props.receipt.person}</span></h2>
       <h3 className="ReceiptOrderMain">Main: <span className="ReceiptDataText">{props.receipt.order.main}</span></h3>
       <h3 className="ReceiptOrderProtein">
         Protein: <span className="ReceiptDataText">{props.receipt.order.protein}</span>
+        
       </h3>
+      <h3 className="ReceiptOrderToppings">Toppings: <span className="ReceiptDataText">{props.receipt.order.toppings.join( ", ")}</span></h3>
       <h3 className="ReceiptOrderRice">Rice: <span className="ReceiptDataText">{props.receipt.order.rice}</span></h3>
       <h3 className="ReceiptOrderSauce">Sauce: <span className="ReceiptDataText">{props.receipt.order.sauce}</span></h3>
       <h3 className="ReceiptOrderDrink">Drink: <span className="ReceiptDataText">{props.receipt.order.drink}</span></h3>
