7ce63474a762ced934bc5ce6fca6e65e7143896e
Jaroslaw Pogoda <jarek1330@gmail.com>
2022-01-16 20:10:27 -0500

    'update

--- students/src/components/Layout.jsx
+++ students/src/components/Layout.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import './Layout.css'
 import Main from "./Main/Main";
+import Header from "./Header/Header";
 function Layout(){
     return (
         <div className="Layout">
+            <Header/>
             <Main/>
         </div>
     );
--- students/src/components/Main/Main.jsx
+++ students/src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import students from "../../Data/Data"
+import Students from "../../Data/Data"
+import Student from './Student/Student'
+import './Main.css'
 export default function Main(){
-    const [students, setStudents] = useState(students)
+    const [students, setStudents] = useState(Students)
     console.log(students)
     return (
         <div className="Main">
-
+            {students.map((student,index)=>{return <Student student={student} key={index}/>})}
         </div>
     )
 }
